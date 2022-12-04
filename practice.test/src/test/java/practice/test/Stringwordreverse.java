42527538035b2a6d5792a00352a52b3ee81e3abf
Madhusudhankms <kmadhusudhan.15@gmail.com>
2022-12-04 20:53:39 +0530

    changed in string word file

--- practice.test/src/test/java/practice/test/Stringwordreverse.java
+++ practice.test/src/test/java/practice/test/Stringwordreverse.java
@@ -5,13 +5,13 @@ public class Stringwordreverse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String s3 ="madhu is a naughty boy";
-		String [] s4 = s3.split(s3);
-		
-		for(int i=0;i<s3.length();i++){
+		String s3 = "madhu is a naughty boy";
+		String[] s4 = s3.split(s3);
+
+		for (int i = 0; i < s3.length(); i++) {
 			
 		}
-		
+
 	}
 
 }
